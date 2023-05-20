6718484bb1f5437e6d47a7cc1ba3176924c18853
YuNiGor <124684684+YuNiGor@users.noreply.github.com>
2023-05-20 07:47:35 +0200

    Add files via upload

---
+++ 001.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employees WHERE department_id = 90;
+SELECT * FROM employees WHERE salary > 5000;
+SELECT * FROM employees WHERE last_name LIKE 'L%';
+SELECT * FROM departments WHERE location_id = 1700;
+SELECT * FROM locations WHERE country_id = 'US';
